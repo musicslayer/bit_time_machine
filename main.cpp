7e08adbe7c87f42bc90dd5ab09a6f6a5a2f4c3e1
musicslayer <musicslayer@gmail.com>
2025-04-02 18:01:36 -0400

    Update

--- main.cpp
+++ main.cpp
@@ -1,31 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <conio.h>
-#include <io.h>
-#include <fcntl.h>
-#include <time.h>
-#include <process.h>
-#include <share.h>
-
+#include <format>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
-#define NUMBEROFSTATES 100
-#define MAXFILESIZE 70000
+const int MAX_FILE_SIZE = 70000; // Bytes
+const int MAX_NUMBER_OF_STATES = 100;
 
-FILE *fp;
-char inputfile[100];
-char outputfile[100];
-char backupfile[100];
-char state[NUMBEROFSTATES][MAXFILESIZE];
-int sizeofstate[NUMBEROFSTATES];
+std::string inputfile;
+std::string outputfile;
+std::string backupfile;
+std::string state[MAX_NUMBER_OF_STATES];
 
 int s = 0;
 int current;
 
 void clearScreen(void);
-void textRead(char[1000]);
+void textRead(std::string);
 void updateDisplay(void);
 
 int main() {
@@ -34,53 +25,46 @@ int main() {
 
         current = 0;
 
-        //memset(backupfile, '\0', 100);
-        backupfile[0] = '\0';
-
         while(true) {
             //Choosing input file
-            printf("Choose a file to track.");
-            scanf("%s", inputfile);
+            std::cout << "Choose a file to track." << std::flush;
 
-            //fp = fopen(inputfile, "rb");
-            fp = _fdopen(_sopen(inputfile, _O_RDONLY ,_SH_DENYNO), "rb");
-            //fp = _fdopen(_sopen(inputfile, _O_RDWR, _SH_DENYNO), "rb");
+            std::getline(std::cin, inputfile);
+
+            std::ifstream file(inputfile, std::ios::binary);
 
             clearScreen();
-            //printf("%i", fp);
-            if(fp != NULL) {
-                fclose(fp);
+            if(file) {
+                file.close();
                 break;
             }
         }
 
         // Opening backup file or creating new backup file
-        strcat(backupfile, inputfile);
-        strcat(backupfile, ".backup");
-
-        //fp = fopen(backupfile, "rb");
-        fp = _fdopen(_sopen(backupfile, _O_RDONLY, _SH_DENYNO), "rb");
-        //fp = _fdopen(_open(backupfile, _O_RDONLY, _O_BINARY), "rb");
+        backupfile = inputfile + ".backup";
+        
+        std::ifstream file(backupfile, std::ios::binary);
 
         // If the backup file exists
-        if(fp != NULL) {
-            fseek(fp, 0, SEEK_SET); 
-            for(int c1 = 0; c1 < NUMBEROFSTATES; c1++) {
-                fscanf(fp, "%i", &sizeofstate[c1]);  
-                fread(&state[c1][0], 1, 1, fp);  
-                for(int c2 = 0; c2 < sizeofstate[c1]; c2++) {
-                    fread(&state[c1][c2], 1, 1, fp);  
+        if(file) {
+            //fseek(fp, 0, SEEK_SET); 
+            for(int c1 = 0; c1 < MAX_NUMBER_OF_STATES; c1++) {
+                //fscanf(fp, "%i", &sizeofstate[c1]);  
+                //fread(&state[c1][0], 1, 1, fp);  
+                for(int c2 = 0; c2 < state[c1].length(); c2++) {
+                    //fread(&state[c1][c2], 1, 1, fp);  
                 }
 
-                if(feof(fp) != 0) {
-                    current = s - 1;
-                    break;
-                }
+                //if(feof(fp) != 0) {
+                //    current = s - 1;
+                //    break;
+                //}
 
                 s++;
             }
 
-            fclose(fp);
+            //fclose(fp);
+            file.close();
         }
 
         // If the backup file doesn't exist
@@ -90,8 +74,9 @@ int main() {
         }
 
         // Choosing output file
-        printf("Choose the output file.");
-        scanf("%s",outputfile);
+        std::cout << "Choose the output file." << std::flush;
+
+        std::getline(std::cin, outputfile);
 
         // Main loop
         while(true) {
@@ -105,8 +90,9 @@ int main() {
                 while(time(NULL) - c < .1);
 
                 textRead(inputfile);
-                if(s < NUMBEROFSTATES && (memcmp(state[s], state[s-1], sizeofstate[s]) !=0 || sizeofstate[s] != sizeofstate[s-1])) {
-                    //printf("Automatically saved state.");
+
+                if(s < MAX_NUMBER_OF_STATES && state[s] != state[s-1]) {
+                    //std::cout << "Automatically saved state." << std::flush;
                     current = s;
                     s++;
 
@@ -114,19 +100,22 @@ int main() {
                     updateDisplay();
                 }
 
-                if(_kbhit()) {
-                    i = _getch();
-                    printf("%c", &i);
+                std::string userInput;
+                std::getline(std::cin, userInput);
+
+                if(!userInput.empty() && userInput.length() == 1) {
+                    i = userInput[0];
+                    std::cout << userInput << std::flush;
                     break;
                 }
             }
 
             if(i == 's') {
-                if(s < NUMBEROFSTATES) {
+                if(s < MAX_NUMBER_OF_STATES) {
                     //Save current input file state to first unused spot
                     textRead(inputfile);
 
-                    //printf("Saved state.");
+                    //std::cout << "Saved state." << std::flush;
                     current = s;
                     s++;
                 }
@@ -137,7 +126,7 @@ int main() {
 
                 while(true) {
                     updateDisplay();
-                    printf("\nSelect a state to restore.");
+                    std::cout << "\nSelect a state to restore." << std::flush;
 
                     // Select state
                     char j = '\n';
@@ -146,29 +135,24 @@ int main() {
                     }
 
                     if(j == '0') {
-                        //current = 0;
-                        fp = fopen(outputfile, "wb");
-                        //fp = _fdopen(_open(outputfile, _O_WRONLY, _O_BINARY), "wb");
-                        //fwrite(state[current], 1, sizeofstate[current], fp);
-                        fwrite(state[0], 1, sizeofstate[0], fp);
-                        fclose(fp);
-                        //printf("Restored state.");
+                        std::ofstream file(outputfile, std::ios::binary);
+                        //fwrite(state[0], 1, sizeofstate[0], fp);
+                        file.close();
+                        std::cout << "Restored state." << std::flush;
                         break;
                     }
                     else {
                         int c1 = atoi(&j);
                         if(c1 < s && c1 > 0) {
                             //current = c1;
-                            fp = fopen(outputfile, "wb");
-                            //fp = _fdopen(_open(outputfile, _O_WRONLY, _O_BINARY), "wb");
-                            //fwrite(state[current], 1, sizeofstate[current], fp);
-                            fwrite(state[c1], 1, sizeofstate[c1], fp);
-                            fclose(fp);
-                            //printf("Restored state.");
+                            std::ofstream file(outputfile, std::ios::binary);
+                            //fwrite(state[c1], 1, sizeofstate[c1], fp);
+                            file.close();
+                            std::cout << "Restored state." << std::flush;
                             break;
                         }
                         else {
-                            //printf("Invalid state.");
+                            std::cout << "Invalid state." << std::flush;
                         }
                     }
                 }
@@ -176,15 +160,14 @@ int main() {
 
             else if(i == 'b') {
                 // Backup all used states to backup file
-                fp = fopen(backupfile, "wb");
-                //fp = _fdopen(_open(backupfile, _O_WRONLY, _O_BINARY), "rb");
+                std::ofstream file(backupfile, std::ios::binary);
                 for(int c1 = 0; c1 < s; c1++) {
-                    fprintf(fp, "%i\n", sizeofstate[c1]);  
-                    for(int c2 = 0; c2 < sizeofstate[c1]; c2++) {
-                        fwrite(&state[c1][c2], 1, 1, fp);
+                    //fprintf(fp, "%i\n", sizeofstate[c1]);  
+                    for(int c2 = 0; c2 < state[c1].length(); c2++) {
+                        //fwrite(&state[c1][c2], 1, 1, fp);
                     }
                 }
-                fclose(fp);
+                file.close();
             }
 
             else if(i == 'q') {
@@ -196,7 +179,7 @@ int main() {
             }
 
             else {
-                //printf("Invalid command.");
+                std::cout << "Invalid command." << std::endl;
             }
         }
     }
@@ -206,43 +189,33 @@ void clearScreen(void) {
     std::cout << "\033[2J\033[H";
 }
 
-void textRead(char filename[1000]) {
+void textRead(std::string filename) {
     // Write contents of file "filename" to the current state
-    fp = _fdopen(_sopen(filename, _O_RDONLY, _SH_DENYNO), "rb");
-    fseek(fp, 0, SEEK_SET); 
+    std::ifstream file(filename, std::ios::binary);
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    state[s] = buffer.str();
 
-    int t4 = 0;
-    while(t4 < MAXFILESIZE) {
-        char ppp;
-        fread(&ppp, 1, 1, fp); 
-        if(feof(fp) != 0) {
-            break;
-        }
-        state[s][t4] = ppp;
-        t4++;
-    }
-
-    sizeofstate[s] = t4;
-    fclose(fp);
+    file.close();
 }
 
 void updateDisplay(void) {
     //Display info
     clearScreen();
 
-    printf("Input file = %s\n", inputfile);
-    printf("Output file = %s\n", outputfile);
-    printf("Backup file = %s\n", backupfile);
-    printf("0");
+    std::cout << std::format("Input file = {}\nOutput file = {}\nBackup file = {}\n0", inputfile, outputfile, backupfile);
 
     for(int c1 = 1; c1 < s; c1++) {
-        printf(" %i", c1);
+        std::cout << std::format(" {}", c1);
     }
-    for(int c1 = s; c1 < NUMBEROFSTATES - 1; c1++) {
-        printf(" X");
+    for(int c1 = s; c1 < MAX_NUMBER_OF_STATES - 1; c1++) {
+        std::cout << " X";
     }
-    printf("\nCurrent state = %i", current);
+
+    std::cout << std::format("\nCurrent state = {}", current);
+
+    std::cout << std::flush;
 
     // Ask user what they want to do
-    printf("\nPress s to save state, r to restore state, b to backup states, q to reset, z to quit.");
+    std::cout << "\nPress s to save state, r to restore state, b to backup states, q to reset, z to quit." << std::endl;
 }
